43bb0d882647ee867768218cd616a1a79aba02ca
jcmoya <juliomoya07@gmail.com>
2021-12-09 15:36:49 +0100

    Updates

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,14 +1,3 @@
-/*
-const path = require('path');
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-};
-*/
-
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
